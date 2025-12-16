c32de278571858b90f302311c8e6f0a0d96ff934
elmeeee <37185272+elmeeee@users.noreply.github.com>
2025-12-16 09:21:08 +0700

    refactor: Remove UIKit SceneDelegate and related app lifecycle files, transition to pure SwiftUI

--- Litrack-MC2/AppDelegate.swift
+++ Litrack-MC2/AppDelegate.swift
@@ -2,35 +2,18 @@
 //  AppDelegate.swift
 //  Litrack-MC2
 //
-//  Created by Karina Widyastuti on 20/05/20.
+//  Legacy Core Data Stack (kept for compatibility)
 //  Copyright © 2020 Karina Widyastuti. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-@UIApplicationMain
+// NOTE: @UIApplicationMain removed - using SwiftUI @main in LitrackApp.swift
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
     // MARK: - Core Data stack
